4db10764ec1ec48b473f58984b3a5eb4c195b860
Vitor Mallis <vitor.mallis@gmail.com>
2025-04-25 12:03:52 -0300

    conversor de real

--- conversorDeMedidas.js
+++ conversorDeMedidas.js
@@ -2,32 +2,28 @@ const medida = prompt("Insira uma medida em metros:")
 
 const unidade = prompt(
     "para qual unidade de medida deseja converter?" +
-    "\n1. milimetro (mm)" +
-    "\n2. centimetro (cm)" +
-    "\n3. decimetro (dm)" +
-    "\n4. decametro (dam)" +
-    "\n5. hectometro (hm)" +
-    "\n6. quilometros (km)"
+    "\n1. Real (R$)" +
+    "\n2. Dolar ($)" +
+    "\n3. Euro (€)" +
+    "\n4. Iene (¥)" +
+    "\n5. Dolar australiano (A$)"
 )
 
 switch (unidade) {
     case "1":
-        alert("Resultado: " + medida + "m = " + medida * 1000 + "mm") 
+        alert("Resultado: " + valor + "R$ = " + valor * 1 + "R$") 
         break
     case "2":
-        alert("Resultado: " + medida + "m = " + medida * 100 + "cm")
+        alert("Resultado: " + valor + "R$ = " + valor * 5,68 + "$")
         break
     case "3":
-        alert("Resultado: " + medida + "m = " + medida * 10 + "dm")
+        alert("Resultado: " + valor + "R$ = " + valor * 6,37 + "€")
         break
     case "4":
-        alert("Resultado: " + medida + "m = " + medida / 10 + "dam")
+        alert("Resultado: " + valor + "R$ = " + valor * 0,39 + "¥")
         break
     case "5":
-        alert("Resultado: " + medida + "m = " + medida / 100 + "hm")
-        break
-    case "6":
-        alert("Resultado: " + medida + "m = " + medida / 1000 + "km")
+        alert("Resultado: " + valor + "R$ = " + valor * 3,63 + "A$")
         break
     default:
         alert("Opção inválida.")
