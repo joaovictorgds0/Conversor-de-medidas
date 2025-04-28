41ca065d3a872837000cad126e46fcbb59e1363d
Vitor Mallis <vitor.mallis@gmail.com>
2025-04-28 09:55:27 -0300

    Conversor de moedas

--- conversorDeMedidas.js
+++ conversorDeMedidas.js
@@ -1,30 +1,30 @@
-const medida = prompt("Insira uma medida em metros:")
+const medida = prompt("Insira um valor em R$:")
 
 const unidade = prompt(
-    "para qual unidade de medida deseja converter?" +
+    "Para qual moeda deseja converter?" +
     "\n1. Real (R$)" +
-    "\n2. Dolar ($)" +
+    "\n2. Dólar ($)" +
     "\n3. Euro (€)" +
     "\n4. Iene (¥)" +
-    "\n5. Dolar australiano (A$)"
+    "\n5. Dólar australiano (A$)"
 )
 
 switch (unidade) {
     case "1":
-        alert("Resultado: " + valor + "R$ = " + valor * 1 + "R$") 
+        alert("Resultado: " + medida + "R$ = " + medida * 1 + "R$") 
         break
     case "2":
-        alert("Resultado: " + valor + "R$ = " + valor * 5,68 + "$")
+        alert("Resultado: " + medida + "R$ = " + medida / 5.68 + "$")
         break
     case "3":
-        alert("Resultado: " + valor + "R$ = " + valor * 6,37 + "€")
+        alert("Resultado: " + medida + "R$ = " + medida / 6.37 + "€")
         break
     case "4":
-        alert("Resultado: " + valor + "R$ = " + valor * 0,39 + "¥")
+        alert("Resultado: " + medida + "R$ = " + medida / 0.39 + "¥")
         break
     case "5":
-        alert("Resultado: " + valor + "R$ = " + valor * 3,63 + "A$")
+        alert("Resultado: " + medida + "R$ = " + medida / 3.63 + "A$")
         break
     default:
         alert("Opção inválida.")
-}
+        }
